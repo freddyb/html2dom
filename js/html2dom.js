d993dc8e919707f1ca2dbc47d23c6d3dccd0a296
freddy <freddy@mozpad.(none)>
2013-04-09 17:15:07 +0200

    added compare feature to see if transform actually worked (it mostly does ;)), switched to using NodeIterators to walk the DOM and removed some bugs

--- js/html2dom.js
+++ js/html2dom.js
@@ -2,106 +2,104 @@ nodes = []
 function html2dom(htmlsource) {
   /*
   * There is no guarantee as to what might happen if the things supplied to html2dom are not valid html.
+  * It's also fairly certain that your html will be mutated. Attributes might shift positions and attribute
+    values without quotation mark will probably get quotes. Single quotes might be turned into double quotes.
   */
-  function nextId(name) {
-      name = name.replace(/[^a-zA-Z]/,"")
-      name = name.toLowerCase(); //XXX use appropriate CamelCase or whatever coding guidelines say
-      cnt++;
-      if (name in ids) {
-          i = ids[name].length;
-          ids[name].push(name +'_'+i);
-          return name +'_'+i
-      }
-      else {
-          ids[name] = [name+'_0'];
-          return name+'_0'
-
-      }
+  function mkId(node) {
+    var name = node.nodeName.replace(/[^a-zA-Z]/,"")
+    name = name.toLowerCase(); //XXX use appropriate CamelCase or whatever coding guidelines say      cnt++;
+    Object.defineProperty(node, "h2d_nodeID", {configurable:true, writable:true}) // this looks like an awful hack. in fact...it is! :/
+    if (name in ids) {
+      i = ids[name].length;
+      node.h2d_nodeID = ids[name] = [name+'_0'];
+      //return ids[name].push(name +'_'+i);
+    }
+    else {
+      node.h2d_nodeID = [name+'_0'];
+      //return ids[name] = [name+'_0'];
+    }
   }
   function escSingleQuote(t) {
-      return t.replace("'", "\'");
+    return t.replace("'", "\'");
   }
   function newElement(node_var,node_name) {
-      src += ("\n"+node_var+ " = document.createElement('" + node_name +"');\n");
+    if (node_name == "script") { //XXX use a more generic way than this hard coded blacklist
+      src += "//XXX CSP will forbid inline JavaScript.\n";
+    }
+    src += ("\n"+node_var+ " = document.createElement('" + node_name +"');\n");
   }
   function newAttribute(node_var, attr,val) {
+    if (attr.indexOf("on") == 0) { //XXX use a more generic way than this hard coded blacklist
+      src += "//XXX CSP will forbid inline JavaScript and event handlers. Use addEventHandler instead!\n";
+    }
+    if (attr == "style") { //XXX use a more generic way than this hard coded blacklist
+      src += "//XXX CSP will forbid inline styles. Use ``"+node_var + ".style'' instead of setAttribute.\n";
+    }
       src += ( node_var+ ".setAttribute(" + attr.toSource() + ","+ val.toSource() +");\n");
   }
   function newText(node_var, text) {
-      src += (node_var+" = document.createTextNode(" +  text.toSource() +");\n");
+    src += (node_var+" = document.createTextNode(" +  text.toSource() +");\n");
   }
   function newComment(node_var, cmt) {
-      src += (node_var+" = document.createComment('" +  cmt +"');\n");
+   src += (node_var+" = document.createComment('" +  cmt +"');\n");
   }
   function appendToParent(par, node) {
-      src += (par+".appendChild("+ node +");\n");
-      }
-
-
-  function walkNodes(parent, nodelist) {
-      console.log('--- '+nodelist.length +" nodes")
-      if (parent == undefined) {
-          src += "docFragment = document.createDocumentFragment(); // contains all gathered nodes\n";
-          parent = "docFragment";
-      }
-      for (var i=0;i<nodelist.length;i++) {
-          var node = nodelist[i];
-          nodes.push(node);
-          var nodeDescr = nodelist[i] +', name: '+ node.nodeName + ', type:' + node.nodeType;
-          if (node.nodeValue != null) {
-              nodeDescr += ', value:' + node.nodeValue.toSource();
-          }
-          console.log("Trying to insert: "+ nodeDescr);
-          if (node.nodeType == Node.ELEMENT_NODE) { // ELEMENT_NODE == 1
-              var node_var = nextId(node.nodeName);
-              newElement(node_var, node.nodeName);
-
-              // let's replace attributes
-              for (var j=0;j<node.attributes.length;j++) {
-                  var a = node.attributes[j].name;
-                  var v = node.attributes[j].value;
-                  newAttribute(node_var, a, v)
-              }
-              // recurse into subtrees: attributes and
-              if (node.hasChildNodes())  {
-                  walkNodes(node_var, node.childNodes);
-              }
-              else {
-                  console.log("no children for: " + nodeDescr );
-              }
-              if (parent != undefined) { appendToParent(parent,node_var); }
-          }
-         else if (node.nodeType == Node.TEXT_NODE) {
-              if (/\S/.test(node.textContent)) {
-                  // skips whitespace-only
-                  var node_var = nextId(node.nodeName);
-                  newText(node_var, node.textContent);
-                  if (parent != undefined) { appendToParent(parent,node_var); }
-              }
-         }
-         else if (node.nodeType == Node.COMMENT_NODE){ // 3
-              var node_var = nextId(node.nodeName);
-              newComment(node_var, node.nodeValue);
-              if (parent != undefined) { appendToParent(parent,node_var); }
-         }
-         else {
-             console.log("Unknown Node Type: " + nodeDescr );
-         }
-
-      }
-      if (typeof nodeDescr != "undefined") { // else we iterated over an empty list
-          console.log("done wakling in node:" + nodeDescr);
-      }
+    src += (par+".appendChild("+ node +");\n");
   }
 
+
+  function walkNodes(root) {
+    var iter = document.createNodeIterator(root);
+    var node;
+    while (node = iter.nextNode()) {
+        if (node.nodeName == "BODY") { continue; }
+        var nodeDescr = node +', name: '+ node.nodeName + ', type:' + node.nodeType;
+        if (node.nodeValue != null) {
+          nodeDescr += ', value:' + node.nodeValue.toSource();
+        }
+        if (node.parentNode == root.parentNode || node.parentNode.nodeName == "BODY") {
+          if (src.indexOf("docFragment") != 0) {
+            src += "docFragment = document.createDocumentFragment(); // contains all gathered nodes\n";
+            var parentName = "docFragment";
+          }
+        }
+        else {
+          parentName = node.parentNode.h2d_nodeID;
+        }
+        if (node.nodeType == Node.ELEMENT_NODE) { // ELEMENT_NODE == 1
+          mkId(node);
+          newElement(node.h2d_nodeID, node.nodeName);
+          // let's replace attributes
+          for (var j=0;j<node.attributes.length;j++) {
+              var a = node.attributes[j].name;
+              var v = node.attributes[j].value;
+              newAttribute(node.h2d_nodeID, a, v)
+          }
+          if (parentName != undefined) { appendToParent(parentName, node.h2d_nodeID); }
+        }
+        else if (node.nodeType == Node.TEXT_NODE) {
+          if (/\S/.test(node.textContent)) {
+            // skips whitespace-only
+            mkId(node);
+            newText(node.h2d_nodeID, node.textContent);
+            if (parentName != undefined) { appendToParent(parentName, node.h2d_nodeID); }
+          }
+        }
+        else if (node.nodeType == Node.COMMENT_NODE){ // 3
+          mkId(node);
+          newComment(node.h2d_nodeID, node.nodeValue);
+          if (parentName != undefined) { appendToParent(parentName, node.h2d_nodeID); }
+        }
+        else {
+         console.log("Unknown Node Type: " + nodeDescr );
+        }
+      }
+    }
   function init(s) {
       cnt = 0;
       ids = {};
       src = ""
-      //if ((typeof document != "undefined") && (document instanceof HTMLDocument)) {
-      //  doc = document.createDocumentFragment(s);
-      //}
-      //else
+      var parentName;
       if (typeof DOMParser == "function") {
           // a bit more heavy-weight and Firefox only (this is OK for b2g things ;))
           var parser = new DOMParser();
@@ -110,12 +108,11 @@ function html2dom(htmlsource) {
       else {
           throw Error("Your JS environment doesn't come with either of the supported parsers (document.createDocumentFragment or DOMParser)");
       }
-      console.log("starting to walk the document");
-      walkNodes(undefined, doc.body.childNodes); // using body because domparser always creates html, head, body
+      walkNodes(doc.body); // using body because domparser always creates html, head, body
       //XXX work around the body thing...
   }
   init(htmlsource);
 
-  returnObj = { res: src, ni : nextId };
+  returnObj = { res: src };
   return returnObj;
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-nodes = []
-
+//nodes = []; // debug
 function parseHTML() {
   x = html2dom(document.querySelector("#html").value);
   document.querySelector("#domjs").value = x.res;
@@ -7,11 +6,13 @@ function parseHTML() {
 function evalParsedHTML() {
   parseHTML();
   eval(x.res);// magic :D
-  while (x=iframe.contentWindow.document.body.firstChild) {
-      iframe.contentWindow.document.body.removeChild(x);
+  while (iframe.contentWindow.document.body.hasChildNodes()) {
+      iframe.contentWindow.document.body.removeChild(
+        iframe.contentWindow.document.body.firstChild
+        );
   }
   iframe.contentWindow.document.body.appendChild(docFragment);
-  document.querySelector("#resulthtml").value = iframe.contentWindow.document.body.outerHTML;
+  document.querySelector("#resulthtml").value = iframe.contentWindow.document.body.innerHTML;
 }
 
 function viewHTML() {
@@ -23,4 +24,23 @@ function tutorial() {
   var tutsrc = '<table border=4 cellspacing=4 cellpadding=4>\n<tr><td bgcolor="#ffff00">yellow stars<td bgcolor="#00ff00">green clovers</tr>\n<tr><td bgcolor="#ff00ff">purple moons<td bgcolor="00ffff">blue something or other...</td></tr>\n</table>';
   document.querySelector("#html").value = tutsrc;
   alert("Now play with the three buttons below 'your' HTML markup.");
+}
+
+function compareNodes() {
+  if (/^\S*$/.test(resulthtml.value) || /^\S*$/.test(html.value)) {
+    alert("One of the HTML textareas is empty..");
+    return;
+  }
+  var r_parser = new DOMParser()
+  var r_doc = r_parser.parseFromString(resulthtml.value, "text/html");
+
+  var i_parser = new DOMParser()
+  var i_doc = i_parser.parseFromString(html.value, "text/html");
+
+  if (i_doc.body.outerHTML == r_doc.body.outerHTML) {
+    alert("All nodes equal, yay :)");
+  }
+  else {
+    alert("Result is not equal to the input HTML :/\nPlease file a bug!");
+  }
 }
