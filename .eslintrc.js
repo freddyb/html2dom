9405d5e3daa6bd247c148a78044f8ff0a83a79b6
Frederik Braun <fbraun@mozilla.com>
2018-01-30 15:46:40 +0100

    Add eslintrc, gitignore, editorconfig, switch to strict mode

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+          "templateStrings": true
+        }
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "warn"
+    }
+};
